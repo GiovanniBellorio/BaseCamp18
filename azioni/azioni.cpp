71f86788a6e13b639f12328f973576ac72416968
GiovanniBellorio <gbellorio95@gmail.com>
2018-10-24 23:05:11 +0200

    Aggiunta soluzione Azioni Finanziarie

---
+++ azioni/azioni.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cassert>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+ifstream in("input.txt");
+ofstream out("output.txt");
+const int N_MAX = 100;
+
+int N;
+int valori[N_MAX];
+
+int main(){
+	int max = 0;
+	in >> N;
+	for(int i = 0; i<N; i++){
+		in >> valori[i];
+	}
+
+	for(int i = 0; i<N-1; i++){
+		if(max < abs(valori[i] - valori[i+1])){
+			max = abs(valori[i] - valori[i+1]);
+		}
+	}
+
+	out << max;
+}
